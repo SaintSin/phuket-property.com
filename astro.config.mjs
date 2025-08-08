743630d987e205ec40fc9077a7d20c7fa30b2be0
StJohn <stjohn@me.com>
2025-08-08 10:22:26 +0100

    Set up Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,15 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   image: {
     responsiveStyles: true,
   },
+
   integrations: [sitemap(), icon()],
-  site: "http://localhost/",
+  site: "http://phuket-property.com/",
+  adapter: netlify(),
 });
